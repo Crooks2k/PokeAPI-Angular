f45854ab380864b97a26a8deb75633e0227564ac
Daniel Veloza <ciberronaldo@hotmail.com>
2023-05-12 02:59:04 -0500

    feat: [core] implementación de componente Loader para el fetching de datos

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layouts/header/header/header.component';
-import { HttpClientModule } from "@angular/common/http"
-import { PokemonService } from './services/pokemon.service';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
